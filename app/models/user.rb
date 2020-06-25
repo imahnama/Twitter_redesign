393423656cec20c2ac98b7146ec38b443c1e7bd9
imahnama <muannama25@gmail.com>
2020-06-25 22:45:18 +0300

    Add user validations/associations to opinions/following models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :opinions, foreign_key: 'AuthorId', class_name: 'Opinion'
   has_many :followed, foreign_key: 'Followerid', class_name: 'Following'
   has_many :followers, foreign_key: 'Followedid', class_name: 'Following'
+
+  validates :Username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
+  validates :FullName, presence: true, length: { minimum: 3, maximum: 100 }
 
   def self.all_users(user_id)
     User.where('id != ?', user_id)
