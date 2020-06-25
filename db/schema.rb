92a51c759f8b3b5523f2cac7514e452922a1a942
imahnama <muannama25@gmail.com>
2020-06-25 22:46:47 +0300

    Add migration for Opinions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_115216) do
+ActiveRecord::Schema.define(version: 2020_06_24_181937) do
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follwerid"
+    t.integer "followedid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.text "Text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "Username"
