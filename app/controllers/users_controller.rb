e6f2668be209a44da786d0778a36dd43ce720d85
imahnama <muannama25@gmail.com>
2020-06-25 22:48:47 +0300

    Fix path error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
   def show
-    @user = User.find(params[:id])
+     @user = User.find(params[:id])
   end
 
   private
