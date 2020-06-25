50896a82aad8642eaadd6feaf7afa56a858fc3c8
imahnama <muannama25@gmail.com>
2020-06-25 22:46:17 +0300

    Add routes for Opinions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :opinions, only: [:index, :create, :destroy]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
