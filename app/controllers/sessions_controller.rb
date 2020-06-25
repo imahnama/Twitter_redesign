fc4418906c4ccd28f6c4e9bc2633731201ee1651
imahnama <muannama25@gmail.com>
2020-06-25 22:49:05 +0300

    remove flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = 'You have logged out'
-    redirect_to root_path
+    redirect_to login_path
   end
 end
