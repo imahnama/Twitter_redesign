6e5a51b93e55d3d5aa8ca8bdf865506e1a592857
imahnama <muannama25@gmail.com>
2020-06-26 17:39:13 +0300

    set the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
   resources :opinions, only: [:index, :create, :destroy]
-  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
