c462c89d2cd82cb888ba68359b8a38f1288be59d
imahnama <muannama25@gmail.com>
2020-06-26 17:38:14 +0300

    Add gravatar_url for images

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def user_gravatar(user)
+    gravatar_id = Digest::MD5::hexdigest(user.Username.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.Username)
+  end
 end
