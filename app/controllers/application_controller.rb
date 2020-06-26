184c2f0d0a337169170d8bf4c0d103a816162f87
imahnama <muannama25@gmail.com>
2020-06-26 17:35:37 +0300

    Update helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?, :authorize
+  helper_method :current_user, :logged_in?, :require_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
-    !current_user.nil?
+    !!current_user
   end
 
-  def authorize
-    redirect_to login_path unless logged_in?
+  def require_user 
+     if !logged_in? 
+      redirect_to login_path 
+     end
   end
+
 end
