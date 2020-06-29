44fd4e9ebd314f8480fa6e2bf0cbc281efb01d41
imahnama <muannama25@gmail.com>
2020-06-29 20:42:17 +0300

    Update gemfile

--- spec/features/opinion_spec.rb
+++ spec/features/opinion_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.feature 'Opinion', type: :feature do
   before :each do
     @user = User.create(FullName: 'Rahma', Username: 'nama')
-    @user = User.create(FullName: 'Phylis', Username: 'rchumbaa')
+    @user = User.create(FullName: 'Phylis', Username: 'chumbaa')
     Opinion.create(AuthorId: @user.id, Text: 'Hello Universe!')
   end
 
   it 'Should create an opinion' do
     visit root_path
-    fill_in 'Username', with: 'messi'
+    fill_in 'Username', with: 'nama'
     click_button 'Log In'
-    fill_in 'opinion_Text', with: 'Hello friends'
+    fill_in 'opinion[Text]', with: 'Hello friends'
     click_button 'Post Opinion'
     expect(page).to have_content('HOME')
   end
