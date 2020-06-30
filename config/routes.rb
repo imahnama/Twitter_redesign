c5299e19f5e94984b91bfe915f0f4ba249eca24a
imahnama <muannama25@gmail.com>
2020-06-30 21:40:08 +0300

    Add routes for followings controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :opinions, only: [:index, :create, :destroy]
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/follow/:followed_id', to: 'followings#create', as: :follow
+  delete '/:followed_id', to: 'followings#destroy', as: :unfollow
   resources :users
 end
