21e880c78b6098121ec33e9a3446e90a709cd789
imahnama <muannama25@gmail.com>
2020-06-30 21:37:16 +0300

    Fix typo

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,4 +1,8 @@
 class Following < ApplicationRecord
-  belongs_to :Follower, foreign_key: 'Followerid', class_name: 'User'
+  belongs_to :Follower, foreign_key: 'Follwerid', class_name: 'User'
   belongs_to :Followed, foreign_key: 'Followedid', class_name: 'User'
+
+
+  validates :Follwerid, uniqueness: { scope: :Followedid }
+  validates :Followedid, uniqueness: { scope: :Followerid }
 end
