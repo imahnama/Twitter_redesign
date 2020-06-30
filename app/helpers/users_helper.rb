5d978f1951577bfde0c2054c882aa618dcfcadcb
imahnama <muannama25@gmail.com>
2020-06-30 21:37:01 +0300

    Add size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
-  def user_gravatar(user)
+  def user_gravatar(user, options = { size: 80 })
     gravatar_id = Digest::MD5.hexdigest(user.Username.downcase)
+    size = options[:size]
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.Username)
   end
