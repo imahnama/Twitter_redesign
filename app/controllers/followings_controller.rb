fe20faa4c971af0147e609298b63aa3342c41eae
imahnama <muannama25@gmail.com>
2020-06-30 21:36:13 +0300

    Change to right path and fix syntax error

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,12 +1,12 @@
 class FollowingsController < ApplicationController
   def create
-    Following.create(followerid: current_user.id, followedid: params[:followed_id])
-    redirect_to user_path(params[:followed_id])
+    Following.create(follwerid: current_user.id, followedid: params[:followed_id])
+    redirect_to opinion_path(params[:followed_id])
   end
 
   def destroy
     @followings = Following.find_friend_to_unfollow(current_user.id, params[:followed_id])
     @followings.destroy
-    redirect_to user_path(params[:followed_id])
+    redirect_to opinion_path(params[:followed_id])
   end
 end
