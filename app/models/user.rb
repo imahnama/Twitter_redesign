5eaf8d57000a2ac3b0a761a1b2d29aab08683f81
imahnama <muannama25@gmail.com>
2020-06-30 21:40:48 +0300

    Run migrations after fixing typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :opinions, foreign_key: 'AuthorId', class_name: 'Opinion'
-  has_many :followed, foreign_key: 'Followerid', class_name: 'Following'
-  has_many :followers, foreign_key: 'Followedid', class_name: 'Following'
+  has_many :followed, foreign_key: 'follwerid', class_name: 'Following'
+  has_many :followers, foreign_key: 'followedid', class_name: 'Following'
 
   validates :Username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
   validates :FullName, presence: true, length: { minimum: 3, maximum: 100 }
