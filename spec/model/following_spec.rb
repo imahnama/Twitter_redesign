102d2d886ce5e303260114e5745af5a1c6f4ffc1
imahnama <muannama25@gmail.com>
2020-07-03 14:54:29 +0300

    Add validations/association test for following model

--- spec/model/following_spec.rb
+++ spec/model/following_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Following, type: :model do
     it { belong_to(:Follower).with_foreign_key(:Followerid).class_name('User') }
     it { belong_to(:Followed).with_foreign_key(:Followedid).class_name('User') }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followed_id) }
+  end
 end
