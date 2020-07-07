f6e033e2b93fc9c107e1c6e318c91353367deef2
imahnama <muannama25@gmail.com>
2020-07-07 15:45:37 +0300

    Add controller tests

--- spec/controllers/opinion_spec.rb
+++ spec/controllers/opinion_spec.rb
@@ -1,16 +1,37 @@
 require 'rails_helper'
 RSpec.describe OpinionsController, type: :controller do
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    @opinion = FactoryBot.create(:opinion)
+    login(@user)
+  end
   describe 'GET #index' do
-    it 'gets all the opinions and render index page' do
+    it 'gets all the user opinions and render index page' do
       get :index
       expect(response).to be_successful
       expect(response).to render_template(:index)
     end
   end
-  describe 'GET #create' do
-    it 'gives the article show page' do
-      get :index
-      expect(response).to render_template(:main)
+
+  describe 'POST #create' do
+    context 'valid params' do
+      it 'creates a new opinion' do
+        post :create, params: { opinion: { Text: 'Sample opinion', AuthorId: @user.id } }
+        expect(response).to redirect_to(opinions_path)
+      end
+    end
+
+    context 'invalid params' do
+      it 'redirects to index page with invalid params' do
+        post :create, params: { opinion: { Text: 1234 } }
+        get :index
+        expect(response).to render_template(:index)
+      end
     end
   end
 end
