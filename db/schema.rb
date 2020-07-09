92b09652e82a829e51e21519bd904ee13f8c28ef
imahnama <muannama25@gmail.com>
2020-07-09 18:40:59 +0300

    updates for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_701_194_748) do
-  create_table 'followings', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['followed_id'], name: 'index_followings_on_followed_id'
-    t.index %w[follower_id followed_id], name: 'index_followings_on_follower_id_and_followed_id', unique: true
-    t.index ['follower_id'], name: 'index_followings_on_follower_id'
+ActiveRecord::Schema.define(version: 2020_07_01_194748) do
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_followings_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
   end
 
-  create_table 'opinions', force: :cascade do |t|
-    t.integer 'AuthorId'
-    t.text 'Text'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "opinions", force: :cascade do |t|
+    t.integer "AuthorId"
+    t.text "Text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'Username'
-    t.string 'FullName'
-    t.text 'Photo'
-    t.text 'CoverImage'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "Username"
+    t.string "FullName"
+    t.text "Photo"
+    t.text "CoverImage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
